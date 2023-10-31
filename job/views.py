917acbb9eeae4920f57ea24d7efa880da0c7e384
AhmedRaafat3 <ahmedmostafarafat22@gmail.com>
2023-10-31 23:25:36 +0200

    add detail

--- job/views.py
+++ job/views.py
@@ -4,3 +4,8 @@ from.models import job
 def job_list(request):
     aLL_Jobs= job.objects.all()
     return render(request,'job/job_list.html',{'jobs':aLL_Jobs})
+
+
+def job_detail(request,slug):
+    Job =job.objects.get(slug=slug)
+    return render (request,'job/job_detail.html',{'Job':Job})
